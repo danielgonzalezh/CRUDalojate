1ce61e52e46e59e37afd629c26bb2c0df5b804d8
tato <buzondaniel@gmail.com>
2022-03-25 17:24:37 -0500

    se trabajo en agregar inmueble y habitacion

--- agregarhabitacion.php
+++ agregarhabitacion.php
@@ -8,12 +8,13 @@
   
   //verificar que el idinmueble exista
   $result = mysqli_query($cnn,"SELECT idinmueble FROM inmueble WHERE idinmueble = '$idinmueble'");
-  if (mysqli_num_rows($result)!=0){
+  $result1 = mysqli_query($cnn,"SELECT idinmueble,nrohabitacion FROM habitacion WHERE idinmueble = '$idinmueble' AND nrohabitacion = '$nrohabitacion'");
+  if (mysqli_num_rows($result)!=0 & mysqli_num_rows($result1)!=0 ){
       //sino
       mysqli_query($cnn,"INSERT INTO habitacion VALUES('$idinmueble','$nrohabitacion','$disponible','$precio')");
       echo "La habitacion se ha guardado correctamente...";
   }
   else{
-      echo"No guarda la habitacion el inmueble(idinmueble) no existe...";
+      echo"No guarda la habitacion el inmueble(idinmueble) no existe o la habitacion esta repetida...";
   }
 ?>
--- agregarinmueble.php
+++ agregarinmueble.php
@@ -1,19 +1,21 @@
 <?php
   include('conexion.php');
   //Tomar la informacion enviada por el front-end
-  $idcliente = $_REQUEST['idcliente']; 
-  $nombre = $_REQUEST['nombre'];
+  $idinmueble = $_REQUEST['idinmueble']; 
+  $direccion = $_REQUEST['direccion'];
   $telefono = $_REQUEST['telefono'];
   $pais = $_REQUEST['pais'];
+  $ciudad = $_REQUEST['ciudad'];
+  $disponible = $_REQUEST['disponible'];
   
-  //verificar que el idcliente no se repita
-  $result = mysqli_query($cnn,"SELECT idcliente FROM cliente WHERE idcliente = '$idcliente'");
+  //verificar que el idinmueble no se repita
+  $result = mysqli_query($cnn,"SELECT idinmueble FROM inmueble WHERE idinmueble = '$idinmueble'");
   if (mysqli_num_rows($result)==0){
       //sino lo encontro por el email
-      mysqli_query($cnn,"INSERT INTO cliente VALUES('$idcliente','$nombre','$telefono','$pais')");
-      echo "El cliente se ha guardado correctamente...";
+      mysqli_query($cnn,"INSERT INTO inmueble VALUES('$idinmueble','$direccion','$telefono','$pais','$ciudad','$disponible')");
+      echo "El idinmueble se ha guardado correctamente...";
   }
   else{
-      echo"El usuario con este idcliente ya existe...";
+      echo"El inmueble con este idinmueble ya existe...";
   }
 ?>
